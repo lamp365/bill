f93e0bf88b0cf3efdf3f24bee6be9d20e0de7129
kevin.liu <791845283@qq.com>
2017-03-01 00:52:36 +0800

    sssss

--- app/home/controllers/Welcome.php
+++ app/home/controllers/Welcome.php
@@ -6,7 +6,7 @@ class Welcome extends MY_Controller {
 	public function  __construct()
 	{
 		parent::__construct();
-
+		$this->load->model('Cat_model');
 	}
 
 	public function index()
@@ -34,19 +34,37 @@ class Welcome extends MY_Controller {
 		$res['data'] = array();
 		if($id){
 			//获取分类
+			$res['data'] = $this->Cat_model->getOneCat(array('id'=>$id));
 		}
 		$this->load->view('welcome/ajax/getCat',$res);
 	}
+
 	public function addCat()
 	{
-		$data['name'] = $this->input->post('cat_name');
-		$data['pid']  = $this->input->post('cat_id');
-
+		$this->load->helper(array('form'));
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('cat_name', '分类名称', 'required');
+		if ($this->form_validation->run() == FALSE)
+		{
+			//提示错误并跳转
+			die('验证失败');
+		}else{
+			$data['name'] = $this->input->post('cat_name');
+			$data['pid']  = $this->input->post('cat_id');
+			if($this->Cat_model->addCat($data)){
+				//提示成功并跳转
+				die('成功过');
+			}else{
+				//提示失败并跳转
+				die('失败过');
+			}
+		}
 	}
 
 	/*
  CREATE TABLE `paybill`(
 `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`uid` int(10) unsigned not null comment '1她的2我的',
 `p_id` INT(10) NOT NULL COMMENT '分类pid',
 `c_id` INT(10) NOT NULL COMMENT '分类id',
 `pay_type` TINYINT(1) NOT NULL DEFAULT 1 COMMENT "1表示支出 2表示收入",
@@ -55,7 +73,7 @@ class Welcome extends MY_Controller {
 `createtime` INT(10) NOT NULL DEFAULT 0 COMMENT '时间'
 )ENGINE=MYISAM DEFAULT CHARSET=utf8;
 
-CREATE TABLE `cat`(
+CREATE TABLE `cat2`(
 `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 `pid` INT(10) UNSIGNED NULL NULL ,
 `name` VARCHAR(32) DEFAULT '',
@@ -64,5 +82,13 @@ CREATE TABLE `cat`(
 PRIMARY KEY (`id`)
 )ENGINE=MYISAM DEFAULT CHARSET=utf8;
 
+CREATE TABLE `cat1`(
+`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`pid` INT(10) UNSIGNED NULL NULL ,
+`name` VARCHAR(32) DEFAULT '',
+`sort` INT(10) DEFAULT 0,
+`createtime` INT(10) DEFAULT 0,
+PRIMARY KEY (`id`)
+)ENGINE=MYISAM DEFAULT CHARSET=utf8;
 	 */
 }
---
+++ app/home/models/Cat_model.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin
+ * Date: 17-3-1
+ * Time: 上午12:17
+ */
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Cat_model extends CI_Model{
+
+    const TABLE = 'cat1';
+
+    public function getOneCatBy($where){
+        $query = $this->db->where($where)->get(self::TABLE);
+        return $query->row_array();
+    }
+
+    public function addCat($data){
+        return $this->db->insert(self::TABLE,$data);
+    }
+
+    public function updateCat($where,$data){
+        return $this->db->where($where)->update(self::TABLE,$data);
+    }
+}
